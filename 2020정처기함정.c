b0628e4752538350e4f76b55d83abef3a48be8a8
Jung Jaewon <58683003+jaewon-jun9@users.noreply.github.com>
2020-11-22 23:45:13 +0900

    Create 2020정처기함정.c
    함정문제 브레이크 없음의 차이

---
+++ 2020정처기함정.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+	int c = 1;
+    switch (3){
+        case 1: c+=3;
+            break;
+        case 2: c++;
+            break;
+        case 3: c=0;
+            break;            
+        case 4: c+=3;
+            break;            
+        case 5: c -=10;
+            break;
+        default: c--;
+            break;
+    }
+    printf("%d",c);
+int d = 1;
+    switch (3){
+        case 1: d+=3;
+        case 2: d++;
+        case 3: d=0;
+        case 4: d+=3;
+        case 5: d -=10;
+        default: d--;
+    }
+    printf("%d",d);
+}
